ae93f502f93f5a7d864dae1a3b53b7f8bfb6a760
Muhammad Zhafran Ammar <zhafran.ammar@suitmedia.com>
2023-03-12 22:38:54 +0700

    Add answer for challenge 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module DTS
+
+go 1.19
--- module-1/challenge-1/main.go
+++ module-1/challenge-1/main.go
@@ -7,9 +7,16 @@ import (
 func main() {
 	i:=21
 	j:=true
+	k:= 123.456
 	fmt.Println(i)
-	fmt.Printf("%T", i)
+	fmt.Printf("%T\n", i)
 	fmt.Println("%")
 	fmt.Println(j)
-	fmt.Println()
+	fmt.Printf("%U\n", []rune("Я"))
+	fmt.Printf("%d\n", 21)
+	fmt.Printf("%o\n", 21)
+	fmt.Printf("%x\n", 15)
+	fmt.Printf("%X\n", 15)
+	fmt.Printf("%f\n", k)
+	fmt.Printf("%E\n", k)
 }
